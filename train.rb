280affcdb47a9356943ceba3aa76b81592068588
MakerSquare Student <students@themakersquare.com>
2013-09-20 14:24:22 -0500

    Adds calculate trip method

--- train.rb
+++ train.rb
@@ -9,6 +9,15 @@ class Train
     @model = model
     @speed = speed
   end
+
+  # Calculate the time that a trip will take, in hours.
+  #
+  # distance - The Integer distance of the trip, in miles.
+  #
+  # Returns the Integer time of the trip.
+  def trip_duration(distance)
+    distance * speed
+  end
 end
 
 # This is to test. 
